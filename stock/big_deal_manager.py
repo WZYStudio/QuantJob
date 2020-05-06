1b2154d8641d43af365fbe33c4e99efbf7579729
wzytjjx <wzytjjx@gmail.com>
2020-05-06 23:49:03 +0800

    10大卖单和10大买单这个，开发得还不错

--- stock/big_deal_manager.py
+++ stock/big_deal_manager.py
@@ -3,12 +3,21 @@ import pandas as pd
 
 
 def get_saleorder_descend_greater_than_100_shou(stock_index, date):
+    pass
+
+
+def get_big_deal_descend_top10(stock_index, date, is_sale=True):
     df = stock_helper.get_df(stock_index, date)
-    df.drop_duplicates(subset=['SaleOrderID'], inplace=True)
-    df.sort_values(by='SaleOrderVolume', ascending=False, kind='quicksort', inplace=True)
+
+    KEY_ID = 'SaleOrderID' if (is_sale) else 'BuyOrderID'
+    KEY_VOL = 'SaleOrderVolume' if (is_sale) else 'BuyOrderVolume'
+
+    df.drop_duplicates(subset=[KEY_ID], keep='last', inplace=True)
+
+    df.sort_values(by=KEY_VOL, ascending=False, kind='quicksort', inplace=True)
     # 注意底下那个只是df的某个列去重了，df本身还是那么多项，所以是不行的！
     # df['SaleOrderID'].drop_duplicates(inplace=True)
-    df_clip = df.head(10)
+    df_clip_origin = df.head(10)
 
     # 底下这个，虽然确实可以除100，但是只剩三列，别的列都没有了..这个不行
     # df_clip = df_clip[['Volume', 'SaleOrderVolume', 'BuyOrderVolume']].div(100)
@@ -17,8 +26,12 @@ def get_saleorder_descend_greater_than_100_shou(stock_index, date):
     # df_clip['Volume'] = df_clip.Volume / 100
 
     # 这个好
-    df_clip[['Volume', 'SaleOrderVolume', 'BuyOrderVolume']] = df_clip[
-        ['Volume', 'SaleOrderVolume', 'BuyOrderVolume']].div(100)
+    # df_clip[['Volume', 'SaleOrderVolume', 'BuyOrderVolume']] = df_clip[
+    #     ['Volume', 'SaleOrderVolume', 'BuyOrderVolume']].div(100)
+
+    df_clip = df_clip_origin.copy()
+    df_clip.loc[:, ['Volume', 'SaleOrderVolume', 'BuyOrderVolume']] = df_clip.loc[:, ['Volume', 'SaleOrderVolume',
+                                                                                      'BuyOrderVolume']].div(100)
 
     # 计算前10大卖单的总和
 
@@ -27,10 +40,19 @@ def get_saleorder_descend_greater_than_100_shou(stock_index, date):
     pd.set_option("display.max_columns", 11)
 
     # print('finish:' + str(df_clip))
+
+    if is_sale:
+        print('前10大卖单总和为:' + str(df_clip.SaleOrderVolume.sum()))
+    else:
+        print('前10大买单总和为:' + str(df_clip.BuyOrderVolume.sum()))
+
     print(df_clip)
 
-    print('前10大卖单总和为:' + str(df_clip.SaleOrderVolume.sum()))
+
+def get_duplex_big_deal_descend_top10(stock_index, date):
+    get_big_deal_descend_top10(stock_index, date)
+    get_big_deal_descend_top10(stock_index, date, False)
 
 
 if __name__ == '__main__':
-    get_saleorder_descend_greater_than_100_shou(600196, '2020-04-28')
+    get_duplex_big_deal_descend_top10('600196', '2020-05-06')
